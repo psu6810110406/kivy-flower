02352401e4751a3f3ff78940f980dd34b67ac272
Arnakorn intawong <6810110424@psu.ac.th>
2026-03-05 15:06:21 +0700

    Add: Configuration file for window settings

---
+++ config.py
@@ -0,0 +1,32 @@
+FLOWER_DATA = {
+    "rose": {
+        "pref_weather": "แดดจัด",
+        "dislike_action": "water_in_rain",
+        "phase_penalty": [5, 10, 20, 50],
+        "base_growth": 10
+    },
+    "tulip": {
+        "pref_weather": "ฝนตก",
+        "dislike_action": "sun_without_water",
+        "phase_penalty": [3, 8, 15, 30],
+        "base_growth": 15
+    },
+    "daisy": {
+        "pref_weather": "เมฆมาก",
+        "dislike_action": "over_fertilize",
+        "phase_penalty": [2, 5, 10, 20],
+        "base_growth": 20
+    },
+    "sunflower": {
+        "pref_weather": "แดดจัด",
+        "dislike_action": "water_in_rain",
+        "phase_penalty": [5, 10, 20, 40],
+        "base_growth": 15
+    },
+    "hibiscus": {
+        "pref_weather": "แดดจัด",
+        "dislike_action": "water_in_rain",
+        "phase_penalty": [4, 8, 15, 35],
+        "base_growth": 12
+    }
+}
